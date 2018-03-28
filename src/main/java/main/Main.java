451b3ab3210105f9ec0f2eab4f00946f71878272
Josué Pradas <jpradas@ucm.es>
2018-03-28 10:26:34 +0200

    branch en hu23 para t10

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,14 @@
 package main;
 
+import negocio.asignatura.Asignatura;
+import negocio.asignatura.SAAsignatura;
+import negocio.asignatura.SAAsignaturaImp;
+
 public class Main {
 	public static void main(String args[]) {
 		System.out.println("Hello World!");
+		/*SAAsignatura saa = new SAAsignaturaImp();
+		Asignatura asignatura = new Asignatura("hola", true);
+		saa.create(asignatura);*/
 	}
 }
