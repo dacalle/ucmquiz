dcb7a69243dff3b7dbab7834f7eb357bde3edfe3
Josué Pradas <jpradas@ucm.es>
2018-03-28 11:13:57 +0200

    Hecho el test de readAll de Asignatura

--- src/test/java/negocio/asignatura/SAAsignaturaTest.java
+++ src/test/java/negocio/asignatura/SAAsignaturaTest.java
@@ -1,5 +1,20 @@
 package negocio.asignatura;
 
-public class SAAsignaturaTest {
+import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
+import org.junit.Test;
+
+public class SAAsignaturaTest {
+	
+	@Test
+	public void ver_asignatura_como_profesor_deberia_devolver_una_lista_de_asignaturas() {
+		List<Asignatura> lista = null;
+		SAAsignatura saa = new SAAsignaturaImp();
+		lista = saa.readAll();
+		
+		assertEquals(null, lista);
+	}
+	
 }
